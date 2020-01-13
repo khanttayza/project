55f7de91e2985bed0b5fd84771b72d9043c6cf89
khanttayza <40440092@live.napier.ac.uk>
2020-01-13 11:18:45 +0630

    update App.java, MongoDB test

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,9 +1,31 @@
 package com.napier.sem;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+
 public class App
 {
     public static void main(String[] args)
     {
-        System.out.println("Boo yah!");
+        // Connect to MongoDB on local system (Docker) - we're using 27000
+        MongoClient mongoClient = new MongoClient("192.168.99.102", 27000);
+        // Get a database - will create when we use it
+        MongoDatabase database = mongoClient.getDatabase("mydb");
+        // Get a collection from the database
+        MongoCollection<Document> collection = database.getCollection("test");
+        // Create a document to store
+        Document doc = new Document("name", "Khant Tay Za")
+                .append("class", "Software Engineering Methods")
+                .append("year", "2019/20")
+                .append("result", new Document("CW", 95).append("EX", 85));
+
+        // Add document to collection
+        collection.insertOne(doc);
+
+        // Check document in collection
+        Document myDoc = collection.find().first();
+        System.out.println(myDoc.toJson());
     }
 }
